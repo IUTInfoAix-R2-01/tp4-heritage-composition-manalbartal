0359b2a8601efca58b33956967ce6791f8f97bf1
Manal <manal.bartal@etu.univ-amu.fr>
2026-02-18 14:30:48 +0100

    LineSub et testLineSub

---
+++ src/tp3exosYaip5/TestLineSub.java
@@ -0,0 +1,10 @@
+package tp3exosYaip5;
+
+public class TestLineSub {
+	   public static void main(String[] args) {
+	      LineSub l1 = new LineSub(0, 0, 3, 4);
+	      System.out.println(l1);
+	      System.out.println("Length: " + l1.getLength());
+	      System.out.println("Gradient: " + l1.getGradient());
+	   }
+	}
